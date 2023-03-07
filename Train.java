a03f344f4fd54f53766274e0b964fbb86dc37638
Dakota Law <dlaw14@smith.edu>
2023-03-06 21:10:16 -0500

    V4 (I think)

--- Train.java
+++ Train.java
@@ -1,3 +1,19 @@
 public class Train {
 
+    // private 'Engine' attribute marked with keyword final to indicate composition
+    // ie: 'private final Engine engine'
+
+    // private array list to keep track of all Car's currently attached
+
+    // constructor 'public Train(FuelType fuelType, double fuelMax, int nCars, int maxCapacity)'
+        // this initializes 'Engine' and 'Car' and stores them
+
+    // accessors including
+        // public Engine getEngine()
+        // public Car getCar(int i) <- returns i'th car
+        // public int getTotalCapacity() <- returns total capacity across all cars
+        // public int seatsRemaining() <- returns # remaining open seats across all cars
+
+    // public void printManifest() to print roster of all passengers on board
+
 }
