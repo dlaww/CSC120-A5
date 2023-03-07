731d709ac8603a4220e2db88b0f88faf8315e55f
Dakota Law <dlaw14@smith.edu>
2023-03-06 19:58:47 -0500

    commit

--- Car.java
+++ Car.java
@@ -1,3 +1,64 @@
+import java.util.ArrayList;
+
 public class Car {
+/* description: private array list storing Passenger currently on board and int for car's max capacity
+ * param: manifest - stores all the passengers in the current car
+ * param: maxCapacity - the maximum capacity of the current train car
+ */
+    private ArrayList<Passenger> manifest;
+    int maxCapacity;
+
+/* description: constructor taking in the car's max capacity, initializing an appropriately sized array list
+ * param: maxCapacity
+ */
+    public Car(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.manifest = new ArrayList<Passenger>(maxCapacity);
+    }
+
+/* accessor-like methods 'public int getCapacity()' and 'public int seatsRemaining()' to return max seats and remaining seats
+ * 
+ */
+    public int getCapacity() {
+        System.out.println("Max number of seats " + maxCapacity);
+        return maxCapacity;
+    }
+
+    public int seatsRemaining() {
+        int seatsRemaining = maxCapacity - manifest.size();
+        System.out.println("Number of seats remaining is " + seatsRemaining) ;
+        return seatsRemaining;
+    }
+
+/* description: 'public void addPassenger(Passenger p)'
+ * 
+ */
+    public void addPassenger(Passenger p) {
+        if (this.manifest.size() < this.maxCapacity) {
+            this.manifest.add(p);
+        }
+        System.out.println(p + " was successfully added to car");
+        }
+
+/* 'public void removePassenger(Passenger p)'
+ * 
+ */
+    public void removePassenger(Passenger p) {
+        if (!this.manifest.contains(p)) { // Not already in car
+            throw new RuntimeException(p + " is not aboard train");
+        }
+        this.manifest.remove(p);
+        System.out.println(p + " was sucessfully removed from train car");
+        }
+
+/* 'public void printManifest()' to print a list of all passengers aboard car or "This car is EMPTY"
+ * 
+ */
+    public void printManifest() {
+        if (this.manifest.size() == 0) {
+            throw new RuntimeException("The car is EMPTY!");
+        }
+        System.out.println(this.manifest);
+    }
 
 }
--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,22 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    /* public void boardCar(Car c) can call c.addPassenger(this) to board a given car
+    if car is full should catch the exception thrown by c.addPassenger
+     * 
+     */
+    public void boardCar(Car c) {
+        try {
+            c.addPassenger(this);
+            this.currentPassengers.add(c);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /* public void getOffCar(Car c) can call c.removePassenger(this) to get off a given car
+    if passenger isn't on board, should catch exception thrown by c.removePassenger
+     * 
+     */
 }
